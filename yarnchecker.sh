859bb1b78184066578dbfb089c3e6be2a7c50c70
otariidae <otariidae@users.noreply.github.com>
2022-09-27 09:53:16 +0900

    add checker script

---
+++ yarnchecker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# check if yarn is installed in ruby builders
+
+july15th=$(date --date 2022-07-15 +%s%3N)
+
+curl --silent "https://us.gcr.io/v2/gae-runtimes/buildpacks/tags/list" | jq -r '.child[] | select(. | startswith("ruby"))' | while read -r runtime; do
+	image_name="us.gcr.io/gae-runtimes/buildpacks/$runtime/builder"
+	curl --silent "https://us.gcr.io/v2/gae-runtimes/buildpacks/$runtime/builder/tags/list" | jq ".manifest[] | select((.timeUploadedMs | tonumber) > $july15th) | .tag[]" | jq -s "sort_by(.)" | jq -r ".[]" | while read -r tag; do
+		echo "$image_name:$tag"
+		docker run --rm "$image_name:$tag" yarn --version
+		sleep 1
+	done
+done
